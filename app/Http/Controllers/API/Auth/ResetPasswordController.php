dc1946372afe71cb5656f56e3b38573f6a7a1312
Mostafa <m@gmail.com>
2024-12-15 18:19:01 +0200

    I made the reset password process

--- app/Http/Controllers/API/Auth/ResetPasswordController.php
+++ app/Http/Controllers/API/Auth/ResetPasswordController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class ResetPasswordController extends Controller
 {
+    public function resetPassword(Request $request)
+    {
+        try {
+            $request->validate([
+                'token' => ['required', 'integer'],
+                'password' => ['required', 'string', 'confirmed', Password::min(8)->mixedCase()],
+            ]);
+            $reset = DB::table('password_reset_tokens')->where('token', $request->token)->first();
+            if (!$reset) {
+                return responseJson(404, 'Invalid OTP.');
+            }
+            $user = User::where('email', $reset->email)->first();
+
+            if (!$user) {
+                return responseJson(404, 'User not found.');
+            }
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+            DB::table('password_reset_tokens')->where('token', $request->token)->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Password reset successfully.',
+            ]);
+        } catch (ValidationException $e) {
+            return responseJson(422, 'Validation failed.', [
+                'first error' => $e->getMessage(),
+                'all errors' => $e->errors()
+            ]);
+        }
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\Auth\ForgotPasswordController;
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\Auth\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,4 @@ Route::get('/', function () {
 Route::post('/register', [RegisterController::class, 'register'])->middleware('throttle:5,1');
 Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:5,1');
 Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->middleware('throttle:5,1');
+Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->middleware('throttle:5,1');
