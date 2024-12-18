7eff62eab1181d395dac74572737522e70d2e7d3
abdo389 <001abo001@gmail.com>
2024-12-18 21:59:17 +0200

    add api trait

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,34 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ReviewRequest;
 use App\Models\Review;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+    use ApiResponse;
+
     function index() {
         $reviews = Review::all();
-        return response([
-            "data" => $reviews,
-            "message" => "all reviews"
-        ]);
+        return $this->success(200, "all reviews", $reviews);
     }
 
     function show(Review $review) {
-        return response($review);
+        return $this->success(200, "review found!", $review);
     }
 
     function store(ReviewRequest $request) {
         Review::create($request->validated());
         return response(["message" => "review created successfully!"]);
+        return $this->success(200, "review created successfully!");
     }
 
     function update(ReviewRequest $request, Review $review) {
         $review->update($request->validated());
-        return response(["message" => "review updated successfully!"]);
+        return $this->success(200, "review updated successfully!");
     }
 
     function destroy(Review $review) {
         $review->delete();
-        return response(["message" => "review deleted successfully!"]);
+        return $this->success(200, "review deleted successfully!");
     }
 }
