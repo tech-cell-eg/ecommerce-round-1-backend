cee41d4dcca2be95e98c790525fbb7a037a33d08
Mostafa Abdelrahman <masaa.5763@gmail.com>
2024-12-19 13:43:33 +0200

    create some tests on trait file control and solving the reviews

--- app/Traits/FileControl.php
+++ app/Traits/FileControl.php
@@ -7,13 +7,14 @@ use Illuminate\Support\Facades\Storage;
 
 trait FileControl
 {
-    use ApiResponse;
-
     /**
      * @throws Exception
      */
     public function uploadFiles($files, $path = 'images/', $disk = 'public'): array
     {
+        if (empty($files)) {
+            throw new Exception('No files provided for uploading.');
+        }
         $uploadedFiles = [];
         $files = is_array($files) ? $files : [$files];
         foreach ($files as $file) {
@@ -28,39 +29,49 @@ trait FileControl
     /**
      * @throws Exception
      */
-    public function deleteFiles($files, $disk = 'public'): void
+    public function deleteFiles($files, $disk = 'public'): array
     {
+        if (empty($files)) {
+            throw new Exception('No files provided for deleting.');
+        }
+        $deletedFiles = [];
         $files = is_array($files) ? $files : [$files];
         foreach ($files as $file) {
             if (!Storage::disk($disk)->exists($file)) {
                 throw new Exception("Files does not exist.");
             }
-            if (!Storage::disk($disk)->delete($file)) {
+            if (!$deletedFiles[] = Storage::disk($disk)->delete($file)) {
                 throw new Exception("Delete files failed.");
             }
         }
+        return $deletedFiles;
     }
 
     /**
      * @throws Exception
      */
-    public function downloadFiles($files, $disk = 'public'): void
+    public function downloadFiles($files, $disk = 'public'): array
     {
+        if (empty($files)) {
+            throw new Exception('No files provided for downloading.');
+        }
+        $downloadedFiles = [];
         $files = is_array($files) ? $files : [$files];
         foreach ($files as $file) {
             if (!Storage::disk($disk)->exists($file)) {
                 throw new Exception("Files does not exist.");
             }
-            if (!Storage::disk($disk)->download($file)) {
+            if (!$downloadedFiles[] = Storage::disk($disk)->download($file)) {
                 throw new Exception("Download files failed.");
             }
         }
+        return $downloadedFiles;
     }
 
     /**
      * @throws Exception
      */
-    public function renameFile($fileName, $fileNewName, $disk = 'public'): void
+    public function renameFile($fileName, $fileNewName, $disk = 'public')
     {
         if (Storage::disk($disk)->exists($fileName)) {
             Storage::disk($disk)->move($fileName, $fileNewName);
