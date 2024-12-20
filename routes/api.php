a0680dddfcfbaaf3950a6d9fa388f57449269766
abdo389 <001abo001@gmail.com>
2024-12-20 17:15:28 +0200

    active delete and show methods

--- routes/api.php
+++ routes/api.php
@@ -60,3 +60,5 @@ Route::get("instagram-stories", [InstagramStoriesController::class, "index"]);
 Route::apiResource("cart", CartController::class);
 
 Route::get("notifications", [NotificationController::class, "index"]);
+Route::get("notifications/{id}", [NotificationController::class, "show"]);
+Route::delete("notifications/{id}", [NotificationController::class, "destroy"]);
